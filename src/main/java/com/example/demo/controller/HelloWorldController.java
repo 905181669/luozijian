a6b8a8745bc4175148bdfa0f2ce7f3cbf00966b7
luozijian <905181669@qq.com>
2019-04-13 18:40:47 +0800

    2019-4-13 18:40

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class HelloWorldController {
 
@@ -11,11 +13,14 @@ public class HelloWorldController {
 	
 	@RequestMapping("/hello")
     public String index() {
-		System.out.println("进来了");
-		
+		System.out.println("进来");
+		System.out.println("进来2");
+        add();
         return "Hello World";
     }
 
-	
+	private void add(){
+	    System.out.println(1);
+    }
 	
 }
