048e9a7cc569232dc25b36d73e81a312e61beff8
luozijian <905181669@qq.com>
2019-04-13 13:00:33 +0800

    2019-4-13 13:00

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,28 +1,17 @@
 package com.example.demo.controller;
 
-import java.util.Optional;
-
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.dao.BaseDao;
-import com.example.demo.domain.BaseCode;
-import com.example.demo.domain.BaseEntity;
-import com.example.demo.service.BaseService;
-
 @RestController
 public class HelloWorldController {
 
-	@Autowired
-	private BaseService baseService;
+
 	
 	@RequestMapping("/hello")
     public String index() {
 		System.out.println("进来了");
-		BaseCode b = (BaseCode) baseService.findById(10L);
 		
         return "Hello World";
     }
