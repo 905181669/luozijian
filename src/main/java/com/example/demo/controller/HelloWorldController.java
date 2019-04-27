c3b9a708b919fd7878d3e7bcb70edb9a517b5bf2
luozijian <905181669@qq.com>
2019-04-27 18:29:38 +0800

    2019-4-27 18:29修改提交

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -16,7 +16,7 @@ public class HelloWorldController {
 	
 	@RequestMapping("/hello")
     public String index() {
-		System.out.println("2019-4-27 18:05修改提交");
+		System.out.println("2019-4-27 18:29修改提交");
         add();
         return "Hello World";
     }
