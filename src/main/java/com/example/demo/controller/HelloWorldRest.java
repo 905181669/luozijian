e51c4661802580c8108cccddade43c9c79dfd93f
luozijian <905181669@qq.com>
2019-04-29 21:33:32 +0800

    2019-4-29 21:33 luozijain_dev分支修改提交

--- src/main/java/com/example/demo/controller/HelloWorldRest.java
+++ src/main/java/com/example/demo/controller/HelloWorldRest.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
 public class HelloWorldRest {
-
+	
+	public static void main(String[] args) {
+		System.out.println("2019-4-29 21:33 luozijain_dev分支修改提交");
+	}
 }
