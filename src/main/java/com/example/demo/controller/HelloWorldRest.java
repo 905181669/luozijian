04daf9800dbd808562e47f27bdee57d665560b0d
luozijian <905181669@qq.com>
2019-05-13 22:21:55 +0800

    2019-4-29 21:20 luozijain_dev分支修改提交

--- src/main/java/com/example/demo/controller/HelloWorldRest.java
+++ src/main/java/com/example/demo/controller/HelloWorldRest.java
@@ -5,5 +5,6 @@ public class HelloWorldRest {
 	public static void main(String[] args) {
 		System.out.println("2019-4-29 22:43 luozijain_dev分支修改提交");
 		System.out.println("2019-5-13 22:09 luozijain_dev分支修改提交");
+		System.out.println("2019-5-13 22:21 luozijain_dev分支修改提交");
 	}
 }
