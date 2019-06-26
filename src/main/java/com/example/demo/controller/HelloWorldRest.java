5455c19ab0b6a80a835d156b970e29e99c8dda53
luozijian <luozijian@zuzuche.com>
2019-06-26 21:36:06 +0800

    初始化项目

--- src/main/java/com/example/demo/controller/HelloWorldRest.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.controller;
-
-public class HelloWorldRest {
-	
-	public static void main(String[] args) {
-
-		System.out.println("2019-5-13 22:44 luozijain_dev分支修改提交");
-	}
-}
--- src/main/java/com/example/demo/domain/User.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.domain;
-
-public class User {
-    private int id;
-    private String username;
-    private String password;
-}
